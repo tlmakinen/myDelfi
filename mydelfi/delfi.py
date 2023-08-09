962d8fc3e4cfad1f3ff6360529496b296bfa1cee
Lucas Makinen <lucas.makinen@gmail.com>
2023-08-09 17:43:22 +0100

    avoid circular imports

--- mydelfi/delfi.py
+++ mydelfi/delfi.py
@@ -10,7 +10,7 @@ import scipy.optimize as optimization
 from scipy.stats import multivariate_normal
 import pickle
 
-from . import priors, ndes, train
+from mydelfi import priors, ndes, train
 
 class Delfi():
 
